03ba9149d530a222444f9fc3e640a88c1dcb41c5
FranciscoVitorAraujo <francisco.vitor@aluno.ufca.edu.br>
2025-09-13 11:33:30 -0300

    Text Input criado

---
+++ src/Components/BatTextInput/BatTextInputStyles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  inputer: {
+    backgroundColor:"#E5BF3C",
+    height: 40,
+    padding: 10,
+    width: "100%",
+    color: "#000",
+    fontSize: 20,
+    textAlign: "center",
+    borderWidth: 1,
+    borderRadius: 5
+  }
+});
--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -3,12 +3,16 @@ import { View } from 'react-native';
 
 import { styles } from './Style';
 import { BatLogo } from '../../Components/Logo/BatLogo';
+import { BatTextInput } from '../../Components/BatTextInput/BatTextInput';
 
 export function Home() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.logoContainer}>
         <BatLogo></BatLogo>
+      </View>
+      <View>
+        <BatTextInput></BatTextInput>
       </View>
     </View>
   );
