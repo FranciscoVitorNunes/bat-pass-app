f9e757fd1b1e926b4f826744390ea579e8e7085b
FranciscoVitorAraujo <francisco.vitor@aluno.ufca.edu.br>
2025-09-13 10:40:27 -0300

    Setup inicial do projeto

---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text, StatusBar } from 'react-native';
+
+import { styles } from './Style';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
