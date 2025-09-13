27291349baea6b9f72ea871422ad5185700652c7
FranciscoVitorAraujo <francisco.vitor@aluno.ufca.edu.br>
2025-09-13 12:53:36 -0300

    Import do Clipboard e finalização da parte lógica

--- src/Components/BatButton/BatButton.tsx
+++ src/Components/BatButton/BatButton.tsx
@@ -4,23 +4,26 @@ import { Text, Pressable, View } from 'react-native';
 import { styles } from './BatButtonStyles';
 import { BatTextInput } from '../BatTextInput/BatTextInput';
 import generatePass from '../../service/password-service';
-
+import * as Clipboard from 'expo-clipboard'
 
 
 export function BatButton() {
 
     const [pass, setPass] = useState("")
 
-    const handlePass = () =>{
+    const handleGeneratePassword = () =>{
         const password = generatePass()
         setPass(password)
+    }
+    const handleCopyPassword = () =>{
+        Clipboard.setStringAsync(pass)
     }
 
   return (
     <>
         <BatTextInput pass={pass}></BatTextInput>
         <Pressable
-            onPress={handlePass}
+            onPress={handleGeneratePassword}
             style={styles.button}
         >
             <Text 
@@ -29,7 +32,7 @@ export function BatButton() {
         </Pressable>
 
         <Pressable
-            onPress={()=>{console.log("presionado...")}}
+            onPress={handleCopyPassword}
             style={styles.button}
         >
             <Text
